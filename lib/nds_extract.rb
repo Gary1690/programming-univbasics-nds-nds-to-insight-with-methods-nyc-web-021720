3fcff83b70bcaef11d9bb6b07e3b455bd0c69303
Gary Alberto Cordero Rosa <garycordero1690@gmail.com>
2020-01-18 23:58:44 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -4,7 +4,7 @@ require 'directors_database'
 # Find a way to accumulate the :worldwide_grosses and return that Integer
 # using director_data as input
 def gross_for_director(director_data)
-  total_director_gross
+  total_director_gross = 0
   director_data[:movies].each do |movie|
     total_director_gross += movie[:worldwide_gross]
   end
